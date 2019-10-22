24300d750f381e18d10e07adf90ca4503bc11a93
judith-youziel <jyouziel20@hotmail.com>
2019-10-22 16:45:38 +0100

    Added the ouput functionality

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const input = document.getElementById('input');
-const output = document.getElementById('output');
+const inPut = document.getElementById('input');
+const outPut = document.getElementById('output');
 
 var inArray = [];
+var numArray = [];
+var opArray = [];
 
 function iPut(x) {
   inArray.push(x);
-  input.textContent = inArray.join('');
+  inPut.textContent = inArray.join('');
+ }
+
+function oPut(x) {
+  if (x != '/' && x != '*' && x != '-' && x != '+' && x != '=') {
+    numArray.push(x);
+    outPut.textContent = numArray.join('');
+  }
+  else {
+    opArray.push(x);
+    numArray = [];
+    outPut.textContent = x;
+  }
+}
+
+function calculate() {
+  //result = 3+5*6-2/4;
+  //outPut.textContent = result;
+  outPut.textContent = 'result!';
 }
 
 function initialise() {
- //oDisplay.textContent = 0;
- input.textContent = '';
+ outPut.textContent = 0;
+ inPut.textContent = '';
  inArray = [];
+ numArray = [];
+ opArray = [];
 }
