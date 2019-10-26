83e8ace3bb466953691dfa1696313ac4c7b7500d
Judith <jyouziel20@hotmail.com>
2019-10-26 11:15:50 +0100

    Almost satisfied user story 11

--- index.js
+++ index.js
@@ -8,15 +8,56 @@ var opArray = [];
 var calcArray = [];
 
 var endCalc = false;
+var disableDecimal = false;
 
 function iPut(x) {
+  if (x == '.') {
+    if (disableDecimal == false) {
+      inArray.push(x);
+      inPut.textContent = inArray.join('');
+    }
+    else {
+    }
+  }
+  else {
   inArray.push(x);
   inPut.textContent = inArray.join('');
+  }
  }
 
 function oPut(x) {
-  if (x != '/' && x != '*' && x != '-' && x != '+') {
+  if (x == '.') {
     if (endCalc == false) {
+    if (disableDecimal == false) {
+      calcArray.push(opArray.pop());
+      opArray = [];
+      calcArray.push(minArray.pop());
+      minArray = [];
+      numArray.push(x);
+      outPut.textContent = numArray.join('');
+      disableDecimal = true;
+    }
+    else {
+    }
+  }
+  else {
+    if (disableDecimal == false) {
+      initialise();
+      calcArray.push(opArray.pop());
+      opArray = [];
+      calcArray.push(minArray.pop());
+      minArray = [];
+      numArray.push(x);
+      outPut.textContent = numArray.join('');
+      disableDecimal = true;
+    }
+    else {
+    }
+  }
+}
+
+  else if (x != '/' && x != '*' && x != '-' && x != '+') {
+    if (endCalc == false || disableDecimal == true) {
     calcArray.push(opArray.pop());
     opArray = [];
     calcArray.push(minArray.pop());
@@ -40,6 +81,7 @@ function oPut(x) {
   else if (x != '/' && x != '*' && x != '+') {
     calcArray.push(numArray.join(''));
     numArray = [];
+    disableDecimal = false;
     minArray.push(x);
     outPut.textContent = x;
     endCalc = false;
@@ -48,6 +90,7 @@ function oPut(x) {
     minArray = [];
     calcArray.push(numArray.join(''));
     numArray = [];
+    disableDecimal = false;
     opArray.push(x);
     outPut.textContent = x;
     endCalc = false;
@@ -57,6 +100,7 @@ function oPut(x) {
 function calculate() {
   calcArray.push(numArray.join(''));
   numArray = [];
+  disableDecimal = false;
   var result = eval(calcArray.join(''));
   calcArray = [];
   calcArray.push(result);
@@ -72,4 +116,5 @@ function initialise() {
  inArray = [];
  numArray = [];
  calcArray = [];
+ disableDecimal = false;
 }
